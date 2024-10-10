871e74a8366cbccd9777a17ca97de65f234be54c
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-10-10 16:48:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    // Check if the element is found
+    if (input) {
+        input.addEventListener('click', clickAlert);
+    }
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+// Call the addingEventListener function to activate the listener
+addingEventListener();
